f79c9342a6e5ee942c26cc8d1a3f6462c9973830
thanhdao <thanhdd1@gmail.com>
2013-03-23 17:38:36 +0700

    Demo

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by iOS12 on 3/23/13.
+//  Copyright (c) 2013 TechMaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by iOS12 on 3/23/13.
+//  Copyright (c) 2013 TechMaster. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
